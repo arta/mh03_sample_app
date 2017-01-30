20247d57b6b00ddfa9c3c023b4d34376166fae9d
Marwell <martin.fencl@gmail.com>
2017-01-30 18:26:04 +0000

    Pass app helper methods tests. Green.

--- test/integration/application_helper_test.rb
+++ test/integration/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionDispatch::IntegrationTest
   test "full title helper" do
-    assert_equal full_title,         'Typo on Rails Tutorial Sample App'
-    assert_equal full_title("Help"), 'Typo | Ruby on Rails Tutorial Sample App'
+    assert_equal full_title,         'Ruby on Rails Tutorial Sample App'
+    assert_equal full_title("Help"), 'Help | Ruby on Rails Tutorial Sample App'
   end
 end
