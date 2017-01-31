06a21d916d3a37b6028dbd98835bd1a9577f9b44
Marwell <martin.fencl@gmail.com>
2017-01-31 17:27:59 +0000

    Test User name, email max lengths. TDD Red.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  
+
   def setup
     @user = User.new name:'Example User', email:'user@example.com'
   end
@@ -14,9 +14,19 @@ class UserTest < ActiveSupport::TestCase
     @user.name = ' '
     assert @user.invalid?
   end
-  
+
   test 'email should be present' do
     @user.email = ' '
+    assert @user.invalid?
+  end
+
+  test 'name should not be too long' do
+    @user.name = 'a' * 51
+    assert @user.invalid?
+  end
+
+  test 'email should not be too long' do
+    @user.email = 'a' * 244 + '@example.com'
     assert @user.invalid?
   end
 end
