fbf39466115ac458ed49c301c7bcafb316ba644f
Marwell <martin.fencl@gmail.com>
2017-02-03 03:20:08 +0000

    Implement flash.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new( user_params )
     if @user.save
-      # Handle a successful save.
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user
     else
       render 'new'
     end
@@ -15,7 +16,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find params[:id]
   end
-  
+
   private
     def user_params
       params.require( :user ).permit( :name, :email, 
