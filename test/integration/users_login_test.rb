a6ece9947236ac29b42a8859c32006ef56f81abf
Marwell <martin.fencl@gmail.com>
2017-02-04 05:14:41 +0000

    Stub and script test catching unwanted flash persistance.

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  # Test to catch unwanted flash persistance:
   # test "the truth" do
-  #   assert true
+    # Visit the login path.
+    # Verify that the new sessions form renders properly.
+    # Post to the sessions path with an invalid params hash.
+    # Verify that the new sessions form gets re-rendered and that a flash message appears.
+    # Visit another page (such as the Home page).
+    # Verify that the flash message doesn’t appear on the new page.
   # end
 end
