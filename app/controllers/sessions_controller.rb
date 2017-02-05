94284bf867c63049d28b12a438167bced032164a
Marwell <martin.fencl@gmail.com>
2017-02-05 02:51:07 +0000

    Rename login helper to log_in. Green.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
 
     if user.try :authenticate, params[:session][:password]
-      login user
+      log_in user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination.'
