3af9e5446bd42234bfc34f96ce64fc5c6df0c3cc
Marwell <martin.fencl@gmail.com>
2017-02-05 17:22:27 +0000

    Use self.method_name to define User class methods.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   has_secure_password
 
   # Returns the hash digest of the given string.
-  def User.digest( string )
+  def self.digest( string )
     cost = ActiveModel::SecurePassword.min_cost ? 
       BCrypt::Engine::MIN_COST :
       BCrypt::Engine.cost
@@ -20,7 +20,7 @@ class User < ApplicationRecord
   end
   
   # Returns a random secure token
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
 
