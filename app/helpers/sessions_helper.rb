3e1481cdf69733254e2e6334ca18be3ad6455eaa
Marwell <martin.fencl@gmail.com>
2017-02-05 23:41:40 +0000

    Redefine sessions helper .current_user to check cookies.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,12 +6,21 @@ module SessionsHelper
 
   def remember( user )
     user.remember
-    cookies.permanent.signed[:id] = user.id
+    cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  # Return user from session or cookie if authenticated
   def current_user
-    @current_user ||= User.find_by( id: session[:user_id] )
+    if user_id = session[:user_id]
+      @current_user = User.find_by id: user_id
+    elsif user_id = cookies.signed[:user_id]
+      user = User.find_by id: user_id
+      if user.try :authenticated?, cookies[:remember_token]
+        log_in user
+        @current_user = user
+      end
+    end
   end
   
   def logged_in?
