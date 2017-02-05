ad9dcd56c5dfcad09b8146477024b5b353d4b84d
Marwell <martin.fencl@gmail.com>
2017-02-05 17:24:14 +0000

    Use class << self to define User class methods.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,17 +11,19 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
 
-  # Returns the hash digest of the given string.
-  def self.digest( string )
-    cost = ActiveModel::SecurePassword.min_cost ? 
-      BCrypt::Engine::MIN_COST :
-      BCrypt::Engine.cost
-    BCrypt::Password.create( string, cost: cost )
-  end
-  
-  # Returns a random secure token
-  def self.new_token
-    SecureRandom.urlsafe_base64
+  class << self
+    # Returns the hash digest of the given string.
+    def digest( string )
+      cost = ActiveModel::SecurePassword.min_cost ? 
+        BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+      BCrypt::Password.create( string, cost: cost )
+    end
+    
+    # Returns a random secure token
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
   def remember
