24fddbec36eb335cc4e0c2074535098450a616c0
Marwell <martin.fencl@gmail.com>
2017-02-22 03:21:15 +0000

    Test layout links for logged in and logged out user. Green.

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users( :michael )
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -11,5 +15,27 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", signup_path
     get signup_path
     assert_select 'title', full_title('Sign up')
+    get users_path
+    assert_redirected_to login_path
+    assert_select "a[href=?]", users_path, count: 0
+    log_in_as @user
+    assert_redirected_to users_path
+    follow_redirect! # must to check template or html elements
+    assert_select "a[href=?]", users_path, count: 1
+    assert_select "a[href=?]", user_path( @user ), count: 2 # menu and index
+    assert_select "a[href=?]", edit_user_path( @user ), count: 1
+    assert_select "a[href=?]", logout_path, count: 1
+    get user_path( @user )
+    assert_template 'users/show'
+    get edit_user_path( @user )
+    assert_template 'users/edit'
+    assert_select "form input" do
+      assert_select "[name=?]", 'user[name]'
+      assert_select "[value=?]", @user.name
+    end
+    delete logout_path
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select "a[href=?]", users_path, count: 0
   end
 end
