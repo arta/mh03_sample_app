9dcab2504b2d43ad5bf9fea19e2e40421e7ec543
Marwell <martin.fencl@gmail.com>
2017-02-23 22:37:45 +0000

    Re-organize User model declarations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
-  
-  before_save { email.downcase! }
+
+  has_secure_password
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, 
@@ -9,7 +10,8 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX }, 
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  has_secure_password
+  
+  before_save { email.downcase! }
 
   class << self
     # Returns the hash digest of the given string.
