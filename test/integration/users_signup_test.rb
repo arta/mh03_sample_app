5f226458353bb0b581f89afb60839eb42b01ede2
Marwell <martin.fencl@gmail.com>
2017-02-25 23:44:44 +0000

    Test user signup, activation, login. Green.

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    # Because the deliveries array is global, we have to reset it in the setup
+    # method to prevent our code from breaking if any other tests deliver email
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -24,21 +31,37 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors', 2
   end
   
-  test 'valid signup information' do
+  test 'valid signup information with account activation' do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name: 'Valid Name',
-                                          email: 'valid@email.com',
-                                          password: 'validpswd',
-                                          password_confirmation: 'validpswd' } }
+      post users_path, params: { user: { name:                  'Example User',
+                                         email:             'user@example.com',
+                                         password:              'password',
+                                         password_confirmation: 'password' } }
     end
-    follow_redirect!
+    assert_equal ActionMailer::Base.deliveries.size, 1
+    # assignes lets us access instance variables in the corresponding action
+    user = assigns( :user )
+    assert_not user.activated?
+    follow_redirect! # although `user` now would now stop existing in the app,
+                     # we can continue using it here as a data container
     assert_template 'static_pages/home'
     assert flash.any?
     assert flash[:info].present?
     assert_select 'div.alert.alert-info', String.present?
     assert_select 'a[href=?]', login_path
-    assert_not is_logged_in? # wouldn't `session[:user_id].nil?` be better here?
+    assert_not is_logged_in?
+    log_in_as user
+    assert_not is_logged_in?
+    get edit_account_activation_path( 'invalid token', email: user.email )
+    assert_not is_logged_in?
+    get edit_account_activation_path( user.activation_token, email: 'wrong@email.com' )
+    assert_not is_logged_in?
+    get edit_account_activation_path( user.activation_token, email: user.email )
+    assert user.reload.activated?
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
   
   test 'signup post route exists' do
