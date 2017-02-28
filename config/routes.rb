28f99e10791d1b1be47b54f186d863953c288399
Marwell <martin.fencl@gmail.com>
2017-02-28 18:36:28 +0000

    Generate PasswordResets controller. Route new, create, edit, update resources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: :edit
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   get    '/signup',  to: 'users#new'
   post   '/signup',  to: 'users#create'
