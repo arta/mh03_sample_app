e0e23100230164064a2e47de4cdc18ba380b08d2
Marwell <martin.fencl@gmail.com>
2017-03-09 21:18:52 +0000

    Implement password resets edit action and view.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,7 +1,26 @@
 class PasswordResetsController < ApplicationController
+  before_action :set_user,                    only: [:edit, :update]
+  before_action :authenticate_activated_user, only: [:edit, :update]
+
+  # GET /password_resets/new
+  #   html:   <a href='/password_resets/new'>..</a>
+  #   rails:  =link_to .. new_password_reset_path
+  #   router: get 'password_resets/new', to: 'password_resets#new'
   def new
   end
+  # Implicitly renders /password_resets/new.html.erb, which contains:
+  #   <form action='/password_resets', method='post' ..>
+  #     <input name='password_reset[email]'..>
+  #     ...
 
+  # POST /password_resets
+  #   html:   <form action='/password_resets', method='post' ..>
+  #             <input name='password_reset[email]' ..>
+  #             ...
+  #   rails:  =form_for :password_reset, url: 'password_resets_path'
+  #             =f.email_field :email
+  #             ...
+  #   router: post '/password_resets', to: 'password_resets#create'
   def create
     user = User.find_by( email: params[:password_reset][:email].downcase )
     if user
@@ -14,7 +33,33 @@ class PasswordResetsController < ApplicationController
       render 'new'
     end
   end
+  # No vieww. Explicitly sends email containing:
+  #   html:   <a href='/password_resets/reset_token/edit?email=escapedemail'>..</a>
 
+  # GET /password_resets/:id/edit?email=some@email.com # :id is assigned a reset_token
+  #   html:   <a href='/password_resets/reset_token/edit?email=escapedemail'>..</a>
+  #   rails:  =link_to .. edit_reset_password_path( @user.reset_token, email: @user.email )
+  #   router: get '/password_resets/:id/edit', to: 'password_resets#edit'
   def edit
   end
+  # Implicitly renders /password_resets/edit.html.erb which contains:
+  #   html:   <form action='/password_resets/reset_token', method='patch'..>
+  #             <input name='email', type='hidden', ..>
+  #             <input name='user[password]', ..>
+  #             <input name='user[password_confirmation]', ..>'
+  #   rails:  =form_for @user, url: password_reset_path( params[:id] ) ..
+  #             =f.hidden_field_tag :email, @user.email
+  #             =f.password_field :password
+  #             =f.password_field :password_confirmation
+
+  private
+    def set_user
+      @user = User.find_by email: params[:email]
+    end
+
+    def authenticate_activated_user
+      redirect_to root_path unless @user &&
+        @user.activated? &&
+        @user.authenticated?( :reset, params[:id] )
+    end
 end
