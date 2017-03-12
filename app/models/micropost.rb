15da7add6a4fdb886a3c52edca85e7308809f106
Marwell <martin.fencl@gmail.com>
2017-03-12 19:32:37 +0000

    Test micropost has a user_id. TDD:R->G.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,5 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true # Redundant? Does `belongs_to :user` run
+                                     # `validates :user, presence: true` ?
 end
