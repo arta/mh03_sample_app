5b05754f24ee8b66fd324fbf2f0ad3d18710f2c1
Marwell <martin.fencl@gmail.com>
2017-03-12 22:25:56 +0000

    Use default_scope to set microposts' default order. TDD:R->G.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+
+  default_scope -> { order( created_at: :desc ) }
+
   validates :user_id, presence: true # Redundant. Declaring `belongs_to :user` 
                                      #  runs `validates :user, presence: true`
                                      #  or some equivalent
