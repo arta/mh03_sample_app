4e78015a8f0050fae987e40b9996fce1af4e53be
Marwell <martin.fencl@gmail.com>
2017-03-12 20:30:59 +0000

    Test micropost content is present and max 140 chars. TDD:R->G.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,5 @@ class Micropost < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true # Redundant? Does `belongs_to :user` run
                                      # `validates :user, presence: true` ?
+  validates :content, presence: true, length: { maximum: 140 }
 end
