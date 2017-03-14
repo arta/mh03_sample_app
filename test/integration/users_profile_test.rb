7ad8be6c35de71ea82aa8a6bc7aed482afffaaf5
Marwell <martin.fencl@gmail.com>
2017-03-14 20:53:12 +0000

    Test /users/show page with user's microposts. Green.

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class UsersProfileTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include ApplicationHelper # to test `full_title()`
+
+  def setup
+    @user = users( :michael )
+  end
+
+  test "profile display" do
+    get user_path( @user )
+    assert_template 'users/show'
+    # Possible due to `include ApplicationHelper` above:
+    assert_select 'title', full_title( @user.name )
+    assert_select 'h1', text: @user.name
+    assert_select 'h1>img.gravatar'
+    assert_match @user.microposts.count.to_s, response.body
+    # Similar to the assert_match above, but much more specific:
+    assert_select 'h3', "#{@user.microposts.count} Microposts"
+    assert_select 'div.pagination'
+    @user.microposts.page( 1 ).each do |micropost|
+      assert_match micropost.content, response.body
+    end
+  end  
 end
