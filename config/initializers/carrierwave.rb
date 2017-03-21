11e81519ef18d3c2d07667374e2b8d8457cf62f7
Marwell <martin.fencl@gmail.com>
2017-03-21 00:55:48 +0000

    Fix a typo(!) in fog credentials to debug failing push to heroku.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider           => 'Rackspace',
-      :rackspace_usernmae => ENV['RACKSPACE_USERNAME'],
+      :rackspace_username => ENV['RACKSPACE_USERNAME'],
       :rackspace_api_key  => ENV['RACKSPACE_API_KEY']
     }
     config.fog_directory = ENV['RACKSPACE_CONTAINER']
