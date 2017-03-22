a1f952695eec4b8e873f3306d63bbe04d6e3a553
Marwell <martin.fencl@gmail.com>
2017-03-22 20:27:37 +0000

    Test Relationship model. Green.

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,4 @@
 class Relationship < ApplicationRecord
-  belongs_to :follower, class_name: 'User'
-  belongs_to :followed, class_name: 'User'
+  belongs_to :follower, class_name: 'User' # Rails 5+ auto-validates presence
+  belongs_to :followed, class_name: 'User' # - // -
 end
--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @relationship = Relationship.new( follower: users( :michael ),
+                                      followed: users( :archer ) )
+  end
+
+  test "should be valid" do
+    assert @relationship.valid?
+  end
+
+  test "should require a follower" do
+    @relationship.follower = nil
+    assert @relationship.invalid?
+  end
+
+  test "should require a followed" do
+    @relationship.followed = nil
+    assert @relationship.invalid?
+  end
 end
