e8c1705fa9dbb708c5e4edbab80494bcc4adc07c
Marwell <martin.fencl@gmail.com>
2017-03-23 14:54:37 +0000

    User has_many followers through passive_relationship. TDD:R->G.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
                                   class_name:  'Relationship',
                                   dependent:   :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships, foreign_key: :followed_id,
+                                   class_name: 'Relationship',
+                                   dependent: :destroy
+  has_many :followers, through: :passive_relationships
   has_secure_password
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -77,6 +81,10 @@ class User < ApplicationRecord
 
   def following?( other_user )
     following.include? other_user
+  end
+
+  def followed_by?( other_user )
+    followers.include? other_user
   end
 
   def follow( other_user )
