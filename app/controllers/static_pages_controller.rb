f5fa32d705ad0d045340081296abb008e65e02a3
Marwell <martin.fencl@gmail.com>
2017-03-24 13:06:51 +0000

    Show user's followees and followers stats.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
+      @user = current_user # for /users/_stats
       @micropost = current_user.microposts.new
       @feed_items = current_user.feed.page params[:page]
     end
   end
-  # Implicitly renders /static_pages/home.html.erb template with
+  # Implicitly renders /static_pages/home with
   #   =render @feed_items
-  #     implicitly renders /microposts/_micropost.html.erb
+  #     implicitly renders /microposts/_micropost
   #     (which has access to a local variable 'micropost')
   #     because @feed_items is a collection of Micropost instances
 
