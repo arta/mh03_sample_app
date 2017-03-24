05de3a1857c8dc6f00fc1a6e0e9d5ff9c735a639
Marwell <martin.fencl@gmail.com>
2017-03-24 11:53:22 +0000

    Route getting resources /users/1/followees and ../followers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    member do
+      get :followees, :followers
+    end
+  end
   resources :account_activations, only: :edit
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
