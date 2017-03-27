23efbea2f06f6140d293347cc439c4f77cb85dfa
Marwell <martin.fencl@gmail.com>
2017-03-27 12:20:17 +0000

    Test follow and unfollow via standard and Ajax ways. Green.

--- test/integration/user_followship_test.rb
+++ test/integration/user_followship_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserFollowshipTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users( :michael )
+    @user  = users( :michael )
+    @other = users( :malory )
     log_in_as( @user )
   end
 
@@ -25,6 +26,34 @@ class UserFollowshipTest < ActionDispatch::IntegrationTest
     assert_match @user.followers.count.to_s, response.body
     @user.followers.each do |user|
       assert_select "a[href=?]", user_path( user )
+    end
+  end
+
+  test "should follow a user the standard way" do
+    assert_difference '@user.followees.count', 1 do
+      post followships_path, params: { followee_id: @other.id }
+    end
+  end
+
+  test "should follow a user with Ajax" do
+    assert_difference '@user.followees.count', 1 do
+      post followships_path, params: { followee_id: @other.id }, xhr: true
+    end
+  end
+
+  test "should unfollow a user the standard way" do
+    @user.follow( @other )
+    followship = @user.outfollowships.find_by( followee_id: @other.id )
+    assert_difference '@user.followees.count', -1 do
+      delete followship_path( followship )
+    end
+  end
+
+  test "should unfollow a user with Ajax" do
+    @user.follow( @other )
+    followship = @user.outfollowships.find_by( followee_id: @other.id )
+    assert_difference '@user.followees.count', -1 do
+      delete followship_path( followship ), xhr: true
     end
   end
 end
